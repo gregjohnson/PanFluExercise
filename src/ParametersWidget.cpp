0c6ad1822f864588a5b7f29027260055ded6970b
Greg Johnson <johnson.gp@gmail.com>
2013-08-26 11:56:51 -0500

    annotations in ParametersWidget.

--- src/ParametersWidget.cpp
+++ src/ParametersWidget.cpp
@@ -15,17 +15,17 @@ ParametersWidget::ParametersWidget()
     // add widgets for all parameters
     constructParameterWidget("R0", "Basic reproduction number", g_parameters.getR0(), 0., WIDGET_MAX_VALUE, SLOT(setR0(double)));
     constructParameterWidget("betaScale", "Scaling factor for beta (transmission rate given contact). beta = R0 / betaScale", g_parameters.getBetaScale(), 0., WIDGET_MAX_VALUE, SLOT(setBetaScale(double)));
-    constructParameterWidget("tau", "exposed -> asymptomatic transition rate", g_parameters.getTau(), 0., WIDGET_MAX_VALUE, SLOT(setTau(double)));
-    constructParameterWidget("kappa", "asymptomatic -> treatable transition rate", g_parameters.getKappa(), 0., WIDGET_MAX_VALUE, SLOT(setKappa(double)));
-    constructParameterWidget("chi", "time spent before progressing from treatable to infectious", g_parameters.getChi(), 0., WIDGET_MAX_VALUE, SLOT(setChi(double)));
-    constructParameterWidget("gamma", "asymptomatic, treatable, or infectious -> recovered transition rate", g_parameters.getGamma(), 0., WIDGET_MAX_VALUE, SLOT(setGamma(double)));
-    constructParameterWidget("nu", "asymptomatic, treatable, or infectious -> deceased transition rate", g_parameters.getNu(), 0., WIDGET_MAX_VALUE, SLOT(setNu(double)));
-    constructParameterWidget("Antiviral effectiveness", "antiviral effectiveness", g_parameters.getAntiviralEffectiveness(), 0., 1., SLOT(setAntiviralEffectiveness(double)));
-    constructParameterWidget("Antiviral adherence", "antiviral adherence", g_parameters.getAntiviralAdherence(), 0., 1., SLOT(setAntiviralAdherence(double)));
+    constructParameterWidget("tau", "exposed -> asymptomatic transition rate (determines average latency period)", g_parameters.getTau(), 0., WIDGET_MAX_VALUE, SLOT(setTau(double)));
+    constructParameterWidget("kappa", "asymptomatic -> treatable transition rate (determines average asymptomatic infectious period)", g_parameters.getKappa(), 0., WIDGET_MAX_VALUE, SLOT(setKappa(double)));
+    constructParameterWidget("Treatment window", "number of days treatable with antivirals", g_parameters.getChi(), 0., WIDGET_MAX_VALUE, SLOT(setChi(double)));
+    constructParameterWidget("gamma", "asymptomatic, treatable, or infectious -> recovered transition rate (determines average infectious period)", g_parameters.getGamma(), 0., WIDGET_MAX_VALUE, SLOT(setGamma(double)));
+    constructParameterWidget("nu", "asymptomatic, treatable, or infectious -> deceased transition rate (determines case fatality rates)", g_parameters.getNu(), 0., WIDGET_MAX_VALUE, SLOT(setNu(double)));
+    constructParameterWidget("Antiviral effectiveness", "probability that an individual treated within the treatment window will recover", g_parameters.getAntiviralEffectiveness(), 0., 1., SLOT(setAntiviralEffectiveness(double)));
+    constructParameterWidget("Antiviral adherence", "proportion of individuals that will seek antiviral treatments", g_parameters.getAntiviralAdherence(), 0., 1., SLOT(setAntiviralAdherence(double)));
     constructParameterWidget("Antiviral capacity", "daily antiviral distribution capacity as a fraction of population", g_parameters.getAntiviralCapacity(), 0., 1., SLOT(setAntiviralCapacity(double)));
-    constructParameterWidget("Vaccine effectiveness", "vaccine effectiveness", g_parameters.getVaccineEffectiveness(), 0., 1., SLOT(setVaccineEffectiveness(double)));
-    constructParameterWidget("Vaccine latency period", "number of days before a vaccination becomes effective", g_parameters.getVaccineLatencyPeriod(), 0, 60, SLOT(setVaccineLatencyPeriod(int)));
-    constructParameterWidget("Vaccine adherence", "vaccine adherence", g_parameters.getVaccineAdherence(), 0., 1., SLOT(setVaccineAdherence(double)));
+    constructParameterWidget("Vaccine effectiveness", "probability that a vaccinated individual will have immunity (after the vaccine effectiveness lag period)", g_parameters.getVaccineEffectiveness(), 0., 1., SLOT(setVaccineEffectiveness(double)));
+    constructParameterWidget("Vaccine effectiveness lag", "number of days until a vaccination reaches maximum effectiveness", g_parameters.getVaccineLatencyPeriod(), 0, 60, SLOT(setVaccineLatencyPeriod(int)));
+    constructParameterWidget("Vaccine adherence", "proportion of individuals seeking vaccination", g_parameters.getVaccineAdherence(), 0., 1., SLOT(setVaccineAdherence(double)));
     constructParameterWidget("Vaccine capacity", "daily vaccine distribution capacity as a fraction of population", g_parameters.getVaccineCapacity(), 0., 1., SLOT(setVaccineCapacity(double)));
 }
 
